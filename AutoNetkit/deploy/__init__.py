e0c2c77ea30c8b2b5f9ead8f9418d57a8d265700
Simon Knight <simon.knight@gmail.com>
2011-11-01 13:26:43 +1030

    remove outdated topology guide

--- AutoNetkit/deploy/__init__.py
+++ AutoNetkit/deploy/__init__.py
@@ -1,2 +1,5 @@
+from AutoNetkit.deploy.netkit_allocate import *
+from AutoNetkit.deploy.netkit_deploy import *
+
 import AutoNetkit.deploy.netkit_allocate
 import AutoNetkit.deploy.netkit_deploy 
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup (
      name = "AutoNetkit",
-     version = "0.5.54",
+     version = "0.5.55",
      description = 'Automated configuration generator for cBGP, Netkit and Junosphere',
      long_description = 'Automated configuration generator for cBGP, Netkit and Junosphere',
 
