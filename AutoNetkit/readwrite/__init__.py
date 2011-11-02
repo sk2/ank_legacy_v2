8396161655f442e62e9cc2d06787a0c03c60d207
Simon Knight <simon.knight@gmail.com>
2011-11-02 14:05:26 +1030

    Fix module errors

--- AutoNetkit/readwrite/__init__.py
+++ AutoNetkit/readwrite/__init__.py
@@ -1,5 +1,7 @@
 from AutoNetkit.readwrite.graphml import *
+from AutoNetkit.readwrite.bgp_parser import *
 from AutoNetkit.readwrite.zoo import *
 
 import AutoNetkit.readwrite.graphml
+import AutoNetkit.readwrite.bgp_parser
 import AutoNetkit.readwrite.zoo
---
+++ AutoNetkit/readwrite/bgp_parser.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+BGP Policy Parser
+"""
+__author__ = "\n".join(['Simon Knight'])
+#    Copyright (C) 2009-2011 by Simon Knight, Hung Nguyen
+
+__all__ = ['parse_policy']
+
+import logging
+LOG = logging.getLogger("ANK")
+
+def parse_policy(network, net_file, default_asn = 1):
+
+    return
+
