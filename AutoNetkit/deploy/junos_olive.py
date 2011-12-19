e2254e91d5840dca608c5c69737fe2c2f9522d34
Simon Knight <simon.knight@gmail.com>
2011-12-19 22:53:18 +1030

    more deployment

--- AutoNetkit/deploy/junos_olive.py
+++ AutoNetkit/deploy/junos_olive.py
@@ -44,6 +44,7 @@ class OliveDeploy():
     """ Deploy a given Junos lab to an Olive Host"""
 
     def __init__(self, host=None, username=None, network=None,
+            base_image = None,
             lab_dir="junos_config_dir"):
         self.server = None    
         self.lab_dir = lab_dir
@@ -55,8 +56,10 @@ class OliveDeploy():
         self.shell_type ="bash"
         self.logfile = open( os.path.join(config.log_dir, "pxssh.log"), 'w')
         self.tap_name = "ank_tap_olive"
-        self.vde_socket_name = "ank_vde_olive"
-        self.vde_mgmt_socket_name = "ank_vde_olive_mgmt"
+        self.vde_socket_name = None
+        self.vde_mgmt_socket_name = None
+        self.base_image = None
+        self.olive_foldername = "ank_olive"
 
         self.local_server = True
         if self.host and self.username:
@@ -121,8 +124,7 @@ class OliveDeploy():
                 LOG.warn("Provided Netkit host is not running Linux")
 
         self.shell = shell   
-        working_directory = self.get_cwd()
-        print "working dir is %s" % working_directory
+        self.working_directory = self.get_cwd()
         return
 
     def transfer_file(self, local_file):
@@ -183,6 +185,35 @@ class OliveDeploy():
                 print "%s not installed" % program
                 return False
             shell.prompt() 
+        shell.prompt() 
+
+    def create_folders(self):
+        shell = self.shell
+        working_directory = self.working_directory
+        self.olive_dir = os.path.join(working_directory, "ank_olive")
+# Update these based on working directory
+        self.vde_socket_name  = os.path.join(self.olive_dir, "ank_vde_olive")
+        self.vde_mgmt_socket_name  = os.path.join(self.olive_dir, "ank_vde_olive_mgmt")
+        self.snapshot_folder = os.path.join(self.olive_dir, "snapshots")
+# need to create this folder if not present
+        self.socket_folder = os.path.join(self.olive_dir, "sockets")
+        required_folders = [self.olive_dir, self.snapshot_folder, self.socket_folder]
+        """
+        for folder in required_folders:
+            print "testing folder", folder
+            chk_cmd = '[ -d %s ] && echo "Present" || echo "Absent"\n\r' % folder 
+            result = self.get_command_output(chk_cmd)
+            if result == "Absent":
+                print "Creating folder %s" % folder
+                shell.sendline("mkdir %s" % folder)
+                shell.prompt() 
+            elif result == "Present":
+                #TODO: convert print to LOGs
+                print "%s exists" % folder
+            else:
+                print "Got unknown folder result of %s" % result
+        """
+
 
     def start_olive(self):
         """ Starts Olives inside Qemu
@@ -194,59 +225,43 @@ class OliveDeploy():
         shell = self.shell
         print "Starting Olives"
 
-        base_image = "/space/base-image.img"
-        snapshot_folder = "/space/snapshots"
-# need to create this folder if not present
-        socket_folder = "/space/sockets"
-        required_folders = [snapshot_folder, socket_folder]
-        for folder in required_folders:
-            chk_cmd = '[ -d %s ] && echo "Present" || echo "Absent"' % folder 
-            result = self.get_command_output(chk_cmd)
-            if result == "Absent":
-                print "Creating folder %s exists" % folder
-                shell.sendline("mkdir %s" % folder)
-                shell.prompt() 
-            elif result == "Present":
-                #TODO: convert print to LOGs
-                print "%s exists" % folder
-
-        return
-
 # transfer over junos lab
 
         tar_file = os.path.join(config.ank_main_dir, self.network.compiled_labs['junos'])
         self.transfer_file(tar_file)
-        print "working dir is %s" % self.get_cwd()
+        configset_directory = os.path.join(self.olive_dir, "configset")
         
 # Tar file copied across (if remote host) to local directory
         shell.sendline("cd ") 
 # Remove any previous lab
-        shell.sendline("rm -rf  " + self.lab_dir)
+        shell.sendline("rm -rf  " + configset_directory)
         shell.prompt() 
-        shell.sendline("mkdir  " + self.lab_dir )
+        shell.sendline("mkdir  " + configset_directory )
         shell.prompt() 
 
         tar_basename = os.path.basename(tar_file)
         
         # Need to force directory to extract to (junosphere format for tar extracts to cwd)
         print "extracting configs"
-        shell.sendline("tar -xzf %s -C %s" % (tar_basename, self.lab_dir))
+        shell.sendline("tar -xzf %s -C %s" % (tar_basename, configset_directory))
 #Need this tar check or all else breaks!
         shell.expect("tar: Removing leading")
         shell.prompt() 
 
-        configset_directory = os.path.join(self.lab_dir, "configset")
         print "configs are in ", configset_directory
         working_directory = self.get_cwd()
+        configset_directory_full_path = os.path.join(working_directory, configset_directory)
 # TODO: store these from junos compiler in network.compiled_labs dict
         config_files = {}
         for node in self.network.graph.nodes():
             node_filename = ank.rtr_folder_name(self.network, node)
             config_files[node] = {}
+            config_files[node]['name'] = node_filename
             config_files[node]['config_file_full_path'] = (os.path.join(working_directory, configset_directory,
                 "%s.conf" % node_filename))
-            config_files[node]['config_file_snapshot'] = os.path.join(snapshot_folder, "%s.iso" % node_filename)
-            config_files[node]['base_image_snapshot'] = os.path.join(snapshot_folder, "%s.img" % node_filename)
+            config_files[node]['config_file_snapshot'] = os.path.join(self.snapshot_folder, "%s.iso" % node_filename)
+            config_files[node]['base_image_snapshot'] = os.path.join(self.snapshot_folder, "%s.img" % node_filename)
+            config_files[node]['monitor_socket'] = os.path.join(configset_directory_full_path, "%s-monitor.sock" % node_filename)
         
         for node, data in config_files.items():
             cmd = "mkisofs -o %s %s " % (data.get('config_file_snapshot'), 
@@ -255,7 +270,7 @@ class OliveDeploy():
             shell.sendline(cmd)
 
         for node, data in config_files.items():
-            cmd =  "qemu-img create -f qcow2 -b %s %s" % (base_image,
+            cmd =  "qemu-img create -f qcow2 -b %s %s" % (self.base_image,
                     data['base_image_snapshot'])
             print cmd
             shell.sendline(cmd)
@@ -266,7 +281,7 @@ class OliveDeploy():
         mac_addresses = self.random_mac_addresses()
         qemu_routers = []
 
-        router_info_tuple = namedtuple('route_info', 'router_name, iso_image, img_image, mac_addresses, telnet_port, switch_socket, monitor_socket')
+        router_info_tuple = namedtuple('router_info', 'router_name, iso_image, img_image, mac_addresses, telnet_port, switch_socket, monitor_socket')
 
         iso_image = "tst.iso"
         img_image = "tst.img"
@@ -274,36 +289,35 @@ class OliveDeploy():
         
         for router in self.network.graph:
             router_info = router_info_tuple(
-                    self.network.label(router),
-                    iso_image,
-                    img_image,
+                    config_files[router].get('name'),
+                    config_files[router].get('config_file_snapshot'),
+                    config_files[router].get('base_image_snapshot'),
 # create 6 mac addresses, the maximum per Olive
                     [mac_addresses.next() for i in range(0,6)],
                     unallocated_ports.next(),
                     self.vde_socket_name,
-                    monitor_socket,
+                    config_files[router].get('monitor_socket'),
                     )
             qemu_routers.append(router_info)
 
+        startup_template = lookup.get_template("autonetkit/olive_startup.mako")
 
+    
+        for router in qemu_routers:
+            startup_command = startup_template.render(
+                    router_info = router
+                    )
 
-        """
-    qemu \
-        -hda ${router_info.img_image} \
-        -hdb ${router_info.iso_image} \         
-            % for mac in router_info.mac_addresses: 
-        -net nic,macaddr=${mac},model=e1000 \
-            % endfor
-        -net vde,sock=${router_info.switch_socket} \
-        -enable-kvm \
-        -serial telnet:127.0.0.1:${router_info.telnet_port},server,nowait,nodelay \
-        -monitor unix:${router_info.monitor_socket},server,nowait \
-        -m 512 m  \
-        -nographic \
-        -localtime \
-        -name ${router_info.router_name} &
-        """
-                                                                    
+# flatten into single line
+            print startup_command
+            startup_command = " ".join(item for item in startup_command.split("\n"))
+
+            shell.sendline(startup_command)
+            shell.prompt()
+            print shell.before
+            print shell.after
+            print
+            sys.exit(0)
 
 
         
@@ -319,15 +333,18 @@ class OliveDeploy():
         print "Starting vde_switch"
 
 # start vde switch
-        start_vde_switch_cmd = "vde_switch -d -t %s -n 2000 -s /tmp/%s -M /tmp/%s" % (self.tap_name, 
+        start_vde_switch_cmd = "vde_switch -d -t %s -n 2000 -s %s -M %s" % (self.tap_name, 
                 self.vde_socket_name, self.vde_mgmt_socket_name)
+        print "start command %s " % start_vde_switch_cmd
         shell.sendline('sudo %s' % start_vde_switch_cmd)
-        i = shell.expect ([ "Address already in use" , pexpect.EOF])
-        if i:
+        i = shell.expect ([ "Address already in use" , "TUNSETIFF: Device or resource busy", pexpect.EOF])
+        if i == 0:
+            print "WARNING: vde_switch already running"
+        elif i == 1:
+            print "WARNING: Device busy, unable to create switch"
+        else:
 # started ok
             pass
-        else:
-            print "WARNING: vde_switch already running"
 
         return
 
@@ -344,9 +361,11 @@ junos_comp = ank.JunosCompiler(inet.network, inet.services, inet.igp)
 junos_comp.initialise()
 junos_comp.configure()
 
-olive_deploy = OliveDeploy(host="trc1", username="sknight", network=inet.network)
+olive_deploy = OliveDeploy(host="trc1", username="sknight", network=inet.network,
+        base_image ="/space/base-image.img")
 olive_deploy.connect_to_server()
-#olive_deploy.check_required_programs()
-#olive_deploy.start_switch()
+olive_deploy.check_required_programs()
+olive_deploy.create_folders()
+olive_deploy.start_switch()
 olive_deploy.start_olive()
 
