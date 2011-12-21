efd81644ac466fef70535adfab24295cb095d6c5
Simon Knight <simon.knight@gmail.com>
2011-12-21 18:53:21 +1030

    fix typo in date

--- AutoNetkit/compiler/junoscompiler.py
+++ AutoNetkit/compiler/junoscompiler.py
@@ -369,7 +369,7 @@ class JunosCompiler:
         LOG.info("Configuring Junos")
         junos_template = lookup.get_template("junos/junos.mako")
         ank_version = pkg_resources.get_distribution("AutoNetkit").version
-        date = time.strftime("%Y%-m-%d %H:%M", time.localtime())
+        date = time.strftime("%Y-%m-%d %H:%M", time.localtime())
 
         physical_graph = self.network.graph
         igp_graph = ank.igp_graph(self.network)
