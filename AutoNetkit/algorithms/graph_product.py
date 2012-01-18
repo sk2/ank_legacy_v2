9b2a6dd949764975cd02e9a88d063eb38541ed23
Simon Knight <simon.knight@gmail.com>
2012-01-18 13:53:55 +1030

    lots more work on graph products - implementation and documentation with doctests and plots

--- AutoNetkit/algorithms/graph_product.py
+++ AutoNetkit/algorithms/graph_product.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 """
-Implement graph products
+Implementation of graph products
+
+Introduction
+==============
+
+Disable plotting of examples in doctests:
+>>> plot = lambda x,y: None
 
 G graph is the undirected base graph
 
@@ -17,21 +23,25 @@ The 'H' attribute of a node in G refers to the key in the 'H_graphs' dictionary:
 >>> H_graphs = {}
 
 >>> H_graphs['style_a'] = nx.trivial_graph()
->>> H_graphs['style_a'].nodes()
-[0]
+>>> plot(H_graphs['style_a'], "style_a")
+
+.. figure::  ../../images/graph_products/style_a.*
 
 >>> H_graphs['style_b'] = nx.cycle_graph(2)
->>> H_graphs['style_b'].edges()
-[(0, 1)]
+>>> plot(H_graphs['style_b'], "style_b")
+
+.. figure::  ../../images/graph_products/style_b.*
 
 >>> H_graphs['style_c'] = nx.cycle_graph(3)
->>> H_graphs['style_c'].edges()
-[(0, 1), (0, 2), (1, 2)]
+>>> plot(H_graphs['style_c'], "style_c")
+
+.. figure::  ../../images/graph_products/style_c.*
 
 >>> H_graphs['style_d'] = nx.Graph( [(0,1), (1,2)])
 >>> H_graphs['style_d'].add_nodes_from( [(0, dict(root=True))])
->>> H_graphs['style_d'].edges()
-[(0, 1), (1, 2)]
+>>> plot(H_graphs['style_d'], "style_d")
+
+.. figure::  ../../images/graph_products/style_d.*
 
 Edges in the graph G specify edges between pops, and have an operator which specifies the interconnection method (covered below). 
 
@@ -44,33 +54,119 @@ Nodes in output graph are the tuple (u,v) for u in G for v in G[u]['H']
 >>> sorted(node_list(G, H_graphs))
 [('a', 0), ('b', 0), ('b', 1), ('c', 0), ('c', 1), ('c', 2)]
 
-Intra PoP links are from the relevant H graph:
->>> sorted(intra_pop_links(G, H_graphs))
-[(('b', 0), ('b', 1)), (('c', 0), ('c', 1)), (('c', 0), ('c', 2)), (('c', 1), ('c', 2))]
 
-Using smaller case study for operator examples:
+Examples
+===========
+
+Using smaller case study for the following examples:
 
 >>> G.clear()
 >>> G.add_nodes_from([ ('a', dict(H='style_d')), ('b', dict(H='style_d'))])
 >>> G.add_edge( 'a', 'b')
 
+>>> plot(G, "G")
+
+.. figure::  ../../images/graph_products/G.*
+
+
+Intra-PoP links
+=========================
+
+Intra PoP links are from the relevant H graph:
+
+>>> sorted(intra_pop_links(G, H_graphs))
+[(('a', 0), ('a', 1)), (('a', 1), ('a', 2)), (('b', 0), ('b', 1)), (('b', 1), ('b', 2))]
+
+>>> plot(nx.Graph(intra_pop_links(G, H_graphs)), "intra_pop_links")
+
+.. figure::  ../../images/graph_products/intra_pop_links.*
+
+
+Inter-PoP links
+=========================
+
+Cartesian Product
+------------------
+
 >>> G['a']['b']['operator'] = 'cartesian'
 >>> inter_pop_links(G, H_graphs)
 [(('a', 0), ('b', 0)), (('a', 1), ('b', 1)), (('a', 2), ('b', 2))]
 
+>>> plot(nx.Graph(inter_pop_links(G, H_graphs)), "cartesian")
+
+.. figure::  ../../images/graph_products/cartesian.*
+
+Rooted Product
+------------------
 
 >>> G['a']['b']['operator'] = 'rooted'
 >>> inter_pop_links(G, H_graphs)
-[]
+[(('a', 0), ('b', 0))]
+>>> plot(nx.Graph(inter_pop_links(G, H_graphs)), "rooted")
+
+.. figure::  ../../images/graph_products/rooted.*
+
+Lexical Product
+------------------
+
+>>> G['a']['b']['operator'] = 'lexical'
+>>> inter_pop_links(G, H_graphs)
+[(('a', 0), ('b', 0)), (('a', 0), ('b', 1)), (('a', 0), ('b', 2)), (('a', 1), ('b', 0)), (('a', 1), ('b', 1)), (('a', 1), ('b', 2)), (('a', 2), ('b', 0)), (('a', 2), ('b', 1)), (('a', 2), ('b', 2))]
+
+>>> plot(nx.Graph(inter_pop_links(G, H_graphs)), "lexical")
+
+.. figure::  ../../images/graph_products/lexical.*
+
+Tensor Product
+------------------
 
 >>> G['a']['b']['operator'] = 'tensor'
 >>> inter_pop_links(G, H_graphs)
-[(('a', 0), ('b', 1)), (('a', 1), ('b', 2)), (('a', 0), ('b', 0))]
+[(('a', 0), ('b', 1)), (('a', 1), ('b', 0)), (('a', 1), ('b', 2)), (('a', 2), ('b', 1))]
+>>> plot(nx.Graph(inter_pop_links(G, H_graphs)), "tensor")
+
+.. figure::  ../../images/graph_products/tensor.*
+
+Strong Product
+------------------
 
 >>> G['a']['b']['operator'] = 'strong'
 >>> inter_pop_links(G, H_graphs)
-[(('a', 0), ('b', 0)), (('a', 1), ('b', 1)), (('a', 2), ('b', 2)), (('a', 0), ('b', 1)), (('a', 1), ('b', 2)), (('a', 0), ('b', 0))]
+[(('a', 0), ('b', 0)), (('a', 1), ('b', 1)), (('a', 2), ('b', 2)), (('a', 0), ('b', 1)), (('a', 1), ('b', 0)), (('a', 1), ('b', 2)), (('a', 2), ('b', 1))]
 
+>>> plot(nx.Graph(inter_pop_links(G, H_graphs)), "strong")
+
+.. figure::  ../../images/graph_products/strong.*
+
+
+Node Attributes
+=========================
+Nodes in AutoNetkit can have attributes. A number of attributes carry special meaning, such as *pop* and *asn*, but the user is free to add their own attributes.
+These attributes are preserved from the G and H graphs.
+
+
+
+Add some extra properties to the G graph defined above:
+
+>>> G.add_nodes_from([ ('a', dict(color='red')), ('b', dict(color='blue'))])
+>>> G.nodes(data=True)
+[('a', {'color': 'red', 'H': 'style_d'}), ('b', {'color': 'blue', 'H': 'style_d'})]
+>>> propagate_node_attributes(G, H_graphs, node_list(G, H_graphs))
+[('a', 0, {'color': 'red'}), ('a', 1, {'color': 'red'}), ('a', 2, {'color': 'red'}), ('b', 0, {'color': 'blue'}), ('b', 1, {'color': 'blue'}), ('b', 2, {'color': 'blue'})]
+
+
+>>> G.add_nodes_from([ ('a', dict(color='red', H='style_e')), ('b', dict(color='blue', H='style_e'))])
+>>> G.nodes(data=True)
+[('a', {'color': 'red', 'H': 'style_e'}), ('b', {'color': 'blue', 'H': 'style_e'})]
+
+
+Define a new H graph with some properties:
+>>> H_graphs['style_e'] = H_graphs['style_d'].copy()
+>>> H_graphs['style_e'].add_nodes_from( [(1, dict(color='green'))])
+>>> H_graphs['style_e'].nodes(data=True)
+[(0, {'root': True}), (1, {'color': 'green'}), (2, {})]
+>>> propagate_node_attributes(G, H_graphs, node_list(G, H_graphs))
+[('a', 0, {'color': 'red'}), ('a', 1, {'color': 'green'}), ('a', 2, {'color': 'red'}), ('b', 0, {'color': 'blue'}), ('b', 1, {'color': 'green'}), ('b', 2, {'color': 'blue'})]
 
 
 """
@@ -104,13 +200,67 @@ def inter_pop_links(G, H_graphs):
         operator = G[u1][u2]['operator']
         H1 = H_graphs[G.node[u1]['H']]
         H2 = H_graphs[G.node[u2]['H']]
+        if operator == 'rooted':
+            edges += [((u1, v1), (u2, v2)) for v1 in H1 for v2 in H2
+                    if H1.node[v1].get("root") == H2.node[v2].get("root") == True ]
+
+        if operator == 'lexical':
+            edges += [((u1, v1), (u2, v2)) for v1 in H1 for v2 in H2]
+
         if operator in cartesian_operators:
             edges += [((u1, v1), (u2, v2)) for v1 in H1 for v2 in H2 if v1 == v2]
         if operator in tensor_operators:
             edges += [((u1, v1), (u2, v2)) for v1 in H1 for v2 in H2
-                    if ( (v1, v2) in H1.edges() or (v1,v2) in H2.edges())]
-        if operator in tensor_operators:
-            edges += [((u1, v1), (u2, v2)) for v1 in H1 for v2 in H2
-                    if H1.node[v1].get("root") == H2.node[v2].get("root") == True ]
+                    if  H1.has_edge(v1, v2) or H2.has_edge(v1,v2)]
 
     return edges
+
+
+def propagate_node_attributes(G, H_graphs, node_list):
+    retval = []
+    for (u,v) in node_list:
+        u_v_data = dict(G.node[u])
+        v_data = dict(H_graphs[u_v_data['H']].node[v])
+        u_v_data.update(v_data)
+# Remove "root" which was used in graph construction - no need to send to AutoNetkit
+        try:
+            del u_v_data['H']
+            del u_v_data['root']
+        except KeyError:
+            pass
+        retval.append( (u, v, u_v_data))
+    return retval
+
+def plot(G, label="plot"):
+    try:
+        import matplotlib.pyplot as plt
+        plt.clf()
+# positions for examples
+        try:
+            labels = dict( (n, "%s%s" % (n[0], n[1])) for n in G)
+            nx.relabel_nodes(G, labels, copy=False)
+            pos = {
+                    ('a0'): (0,2),
+                    ('a1'): (0,1),
+                    ('a2'): (0,0),
+                    ('b0'): (1,2),
+                    ('b1'): (1,1),
+                    ('b2'): (1,0),
+                    }
+        except (IndexError, TypeError):
+# Use spring layout - can't do in a row or won't show cycles (due to overlap)
+                pos = nx.spring_layout(G)
+
+        nx.draw(G, pos, node_color='#A0CBE2', node_size=500, width=1)
+        fig = plt.gcf()
+        fig.set_size_inches(4,4)
+        plt.savefig("%s.pdf" % label)
+        plt.savefig("%s.png" % label, dpi=72,
+                bbox_inches='tight',
+                pad_inches=0.1,
+                )
+    except ImportError:
+# No matplotlib, don't fail the other tests due to this
+        pass
+
+
--- doc/source/reference/examples/examples.rst
+++ doc/source/reference/examples/examples.rst
@@ -3,11 +3,9 @@ Examples
 
 Single AS:
 
-.. image:: ../../images/examples/singleas.*       
 
 Multi AS:
 
-.. image:: ../../images/examples/multias.*       
 
 .. automodule:: AutoNetkit.examples.examples
    :members:
