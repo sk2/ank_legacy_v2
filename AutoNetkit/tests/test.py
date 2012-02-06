87ca1439bcd46b067207864da7e63fce46f23e23
Simon Knight <simon.knight@gmail.com>
2012-02-06 20:49:36 +1030

    added verbose debug

--- AutoNetkit/tests/test.py
+++ AutoNetkit/tests/test.py
@@ -12,14 +12,28 @@ def test_dumps():
     inet.compile()
     inet.dump()
 
-    f_bgp = os.path.join(config.log_dir, "bgp.txt")
-    test_bgp = open(f_bgp, "r").read()
-    master_bgp = open(os.path.join(master_dir, "bgp.txt"), "r").read()
+    f_test = os.path.join(config.log_dir, "bgp.txt")
+    test_file = open(f_test, "r").read()
+    master_file = open(os.path.join(master_dir, "bgp.txt"), "r").read()
 
-    assert(test_bgp == master_bgp)
+    try:
+        assert(test_file == master_file)
+    except AssertionError:
+        message = ''.join(difflib.ndiff(test_file.splitlines(True),
+            master_file.splitlines(True)))
+        LOG.warn(message)
+        raise AssertionError
 
-    f_phys = os.path.join(config.log_dir, "physical.txt")
-    test_phys = open(f_phys, "r").read()
-    master_phys = open(os.path.join(master_dir, "physical.txt"), "r").read()
+    f_test = os.path.join(config.log_dir, "physical.txt")
+    test_file = open(f_test, "r").read()
+    master_file = open(os.path.join(master_dir, "physical.txt"), "r").read()
 
-    assert(test_phys == master_phys)
+    try:
+        assert(test_file == master_file)
+    except AssertionError:
+        message = ''.join(difflib.ndiff(test_file.splitlines(True),
+            master_file.splitlines(True)))
+        LOG.warn(message)
+        raise AssertionError
+
+
