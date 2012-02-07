22f0457530427b9ebff5c789fe53ed33695003b8
Simon Knight <simon.knight@gmail.com>
2012-02-07 16:55:39 +1030

    closes gh-213

--- AutoNetkit/algorithms/tests/test_dns.py
+++ AutoNetkit/algorithms/tests/test_dns.py
@@ -8,12 +8,13 @@ import difflib
 
 
 def test_dns():
-    return
-    config.settings['DNS']['hierarchical'] = True
-    master_dir = (resource_filename(__name__, "netkit"))
-    inet = AutoNetkit.internet.Internet("multias") 
-    inet.compile()
-    config.settings['DNS']['hierarchical'] = False
+    try:
+        config.settings['DNS']['hierarchical'] = True
+        master_dir = (resource_filename(__name__, "netkit"))
+        inet = AutoNetkit.internet.Internet("multias", netkit=True) 
+        inet.compile()
+    finally:
+        config.settings['DNS']['hierarchical'] = False
     #TODO: see why DNS persists across multiple instances, if compiling for Netkit
 
     return
