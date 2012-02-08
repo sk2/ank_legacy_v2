26f3c2818d3ecde2538577d24184b12d291827a0
Simon Knight <simon.knight@gmail.com>
2012-02-08 17:56:42 +1030

    added netkit deploy testing

--- AutoNetkit/deploy/tests/test.py
+++ AutoNetkit/deploy/tests/test.py
@@ -7,17 +7,17 @@ LOG = logging.getLogger("ANK")
 import difflib
 
 def test_netkit_deploy():
-    #TODO: get nosetests to use ssh key to log into deployment machine.... or simulate using pexpect filehandles
-    return
     config.settings['Netkit Hosts'] = {
             'trc1': {
                 "host": "trc1",
                 "username": "autonetkit",
                 "active": True,
+                'collect data': True,
+                'collect data commands': ['zebra: show ip route'],
                 }
             }
     inet = AutoNetkit.internet.Internet("2routers", netkit=True, deploy=True) 
     inet.compile()
     inet.deploy()
-
-   
+    inet.collect_data()
+#TODO: get feedback that machine started ok - need to store in the netkit server
